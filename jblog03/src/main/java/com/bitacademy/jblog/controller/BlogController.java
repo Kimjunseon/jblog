83d022b37884c6860e1af60084ac1e635280ca20
Kimjunseon <junseon0722@gmail.com>
2022-11-29 16:41:30 +0900

    jblog

---
+++ jblog03/src/main/java/com/bitacademy/jblog/controller/BlogController.java
@@ -0,0 +1,42 @@
+package com.bitacademy.jblog.controller;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+/**
+ * 
+ * id:(?!assets)  <- assets이 아닌 .* <- 모든 문자} assets이 아닌 모든 문자는 해당 맵핑으로 이동
+ * 
+ */
+@RequestMapping("/{id:(?!assets).*}")
+public class BlogController {
+	
+	@RequestMapping({"", "/{pathNo1}", "/{pathNo1}/{pathNo2}"})
+	public String index(
+		@PathVariable("id") String id,
+		//Optional<> null값이 생성되지 않음
+		@PathVariable("pathNo1") Optional<Long> pathNo1,
+		@PathVariable("pathNo2") Optional<Long> pathNo2) {
+		Long categoryNo = 0L;
+		Long postNo = 0L;
+		
+		if(pathNo1.isPresent()) {
+			categoryNo = pathNo1.get();
+		} else if (pathNo2.isPresent()) {
+			categoryNo = pathNo1.get();
+			postNo = pathNo2.get();
+		}
+		
+		return "blog/index";
+	}
+	
+	@RequestMapping({"/admin","/admin/basic"})
+	public String adminBasic(@PathVariable("id") String id) {
+		return "blog/admin-basic";
+		
+	}
+}
