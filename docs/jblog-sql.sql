9002b5ae49dab7b422653cefb6db3b4713b8a253
Kimjunseon <junseon0722@gmail.com>
2022-12-12 18:13:32 +0900

    ㅇㅇ

--- docs/jblog-sql.sql
+++ docs/jblog-sql.sql
@@ -10,9 +10,14 @@ select * from blog;
 select * from category;
 select * from post;
 
+  select *
+    from user a, blog b, category c
+   where b.id = a.id
+     and c.id = b.id;
 
-delete from blog;
+select a.name, b.title, b.profile, c.title, c.no, d.title, d.contents, d.reg_date, d.no
+  from user a, blog b, category c, post d
+ where b.id = a.id
+   and c.id = b.id
+   and d.catagory_no = c.no;
 
-select no, title, descr
-  from category
-  order by no desc;
